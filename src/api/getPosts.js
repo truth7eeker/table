16201e292d0a427287024d28d1fcdd8a5bb8bb51
Elvie1997 <el__1997@mai.ru>
2022-05-02 12:59:01 +0300

    change http to https in jsonplaceholder

--- src/api/getPosts.js
+++ src/api/getPosts.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 
     export const getPosts = createAsyncThunk('posts/getPosts', async (firstPost) => {
-    const response = await fetch(`http://jsonplaceholder.typicode.com/posts?_start=${firstPost}&_limit=10`)
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=${firstPost}&_limit=10`)
     const data = await response.json()
     return data
 })
